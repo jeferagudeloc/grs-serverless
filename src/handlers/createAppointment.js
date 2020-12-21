7cb1945d9ed3311827664315eb6b6d3eb91c7c70
Jeferson Agudelo <jeferson.agudelo@karibu.cl>
2020-12-21 20:51:32 -0300

    [FEA] Serverless base, first methods create and get appointment

---
+++ src/handlers/createAppointment.js
@@ -0,0 +1,34 @@
+import {v4 as uuid} from 'uuid'
+import AWS from 'aws-sdk'
+import commonMidleware from '../lib/commonMiddleware'
+import createError from 'http-errors'
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+async function createAppointment(event, context) {
+  
+  const body = event.body;
+
+  const appointment = {
+    id: uuid(),
+    status: 'OPENED',
+    date: body.date,
+    observation: body.observation
+  }
+
+  try {
+    await dynamodb.put({
+      TableName: process.env.APPOINTMENT_TABLE_NAME,
+      Item: appointment
+    }).promise()  
+  } catch (error) {
+    console.log(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({ appointment }),
+  };
+}
+
+export const handler = commonMidleware(createAppointment);
---
+++ src/handlers/getAppointment.js
@@ -0,0 +1,38 @@
+import AWS from 'aws-sdk'
+import commonMidleware from '../lib/commonMiddleware'
+import createError from 'http-errors'
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+export async function getAppointmentById(id) {
+    let appointment;
+
+    try {
+        const result = await dynamodb.get({
+            TableName: process.env.APPOINTMENT_TABLE_NAME,
+            Key: { id }
+        }).promise();
+        console.log("result:", result);
+        appointment = result.Item;
+    } catch (error) {
+        throw new createError.InternalServerError(error);
+    }
+
+    if (!appointment) {
+        throw new createError.NotFound(`Id ${id} not found`);
+    }
+    return appointment;
+}
+
+async function getAppointment(event, context) {
+
+    const { id } = event.pathParameters;
+    let appointment = await getAppointmentById(id);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ appointment }),
+      };
+}
+
+export const handler = commonMidleware(getAppointment);
